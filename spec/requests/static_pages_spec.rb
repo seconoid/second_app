91b6bc20d7872bdb990d1222d4544f4dc3beffef
seconoid <seconsecond@gmail.com>
2015-10-04 23:28:20 +0900

    Done practice No.5.6

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,32 +4,47 @@ describe "StaticPages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_content(heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
-
-    it { should have_content ('Second App') }
-    it { should have_title(full_title('')) }
-    it { should_not have_title ('| Home') }
+    let(:heading)     { 'Second App' }
+    let(:page_title)  { '' }
   end
 
   describe "Help page" do
     before { visit help_path }
-
-    it { should have_content ('Help') }
-    it { should have_title(full_title('Help')) }
+    let(:heading)     { 'Help' }
+    let(:page_title)  { 'Help' }
   end
 
   describe "About page" do
     before { visit about_path }
-
-    it { should have_content ('About') }
-    it { should have_title(full_title('About Us')) }
+    let(:heading)     { 'About' }
+    let(:page_title)  { 'About Us' }
   end
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading)     { 'Contact' }
+    let(:page_title)  { 'Contact' }
+  end
 
-    it { should have_content ('Contact') }
-    it { should have_title(full_title('Contact')) }
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "Second App"
+    expect(page).to have_title(full_title(''))
   end
 end
