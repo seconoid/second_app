99b2b9175a572eee29dc5bcb0639d36fcab5a428
seconoid <seconsecond@gmail.com>
2016-03-12 19:53:14 +0900

    done exercise in chapter 9

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -15,8 +15,14 @@ describe "AuthnticationPages" do
   	before { visit signin_path }
 
   	describe "with invalid information" do
+      let(:user) { FactoryGirl.create(:user) }
   		before { click_button "Sign in" }
 
+      it { should_not have_title(user.name) }
+      it { should_not have_link('Users',       href: users_path) }
+      it { should_not have_link('Profile',      href: user_path(user)) }
+      it { should_not have_link('Settings',     href: edit_user_path(user)) }    
+      it { should_not have_link('Sign out',     href: signout_path) }
   		it { should have_title('Sign in') }
   		it { should have_selector('div.alert.alert-error', text: 'Invalid') }
 
@@ -52,15 +58,24 @@ describe "AuthnticationPages" do
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
-          fill_in "Email",    with: user.email
-          fill_in "Password", with: user.password
-          click_button "Sign in"
+          sign_in user
         end
 
         describe "after signing in" do
 
           it "should render the desired protected page" do
             expect(page).to have_title('Edit user')
+          end
+
+          describe "when signing in again" do
+            before do
+              delete signout_path
+              sign_in user
+            end
+
+            it "should render the default (profile) page" do
+              expect(page).to have_title(user.name)
+            end
           end
         end
       end
@@ -109,6 +124,18 @@ describe "AuthnticationPages" do
 
       describe "submitting a DELETE request to the Users#destroy action" do
         before { delete user_path(user) }
+        specify { expect(response).to redirect_to(root_path) }
+      end
+    end
+
+    describe "as an admin user" do
+      let(:admin) { FactoryGirl.create(:admin) }
+      before do
+        sign_in admin, no_capybara: true
+      end
+
+      describe "submitting a DELETE request to the Users#destroy action" do
+        before { delete user_path(admin) }
         specify { expect(response).to redirect_to(root_path) }
       end
     end
